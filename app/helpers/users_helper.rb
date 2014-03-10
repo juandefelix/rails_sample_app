c0326bd9a0d1cd73797898e2757fa68e0ecf3cca
Juan Ortiz <juandefelix@yahoo.es>
2014-03-10 18:56:26 -0500

    Begining of chap. 7.3 displaying form for signup

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,11 @@
 module UsersHelper
+  # Returns the gravatar (http://gravatar.com) for the given user.
+  def gravatar_for(user)
+    # Creates a MD5 hash from you email
+    gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+    # A URL created from your gravatar_id
+    gravatar_url = "http://secure.gravatar.com/avatar/#{gravatar_id}"
+    # The  image tag  from your gravatar URL
+    image_tag(gravatar_url, alt: user.name, class: "gravatar")
+  end
 end
