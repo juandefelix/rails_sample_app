4426ed7d65d1d1ac0d2ac0a7116d64caef26edaa
Juan Ortiz <juandefelix@yahoo.es>
2014-03-13 13:52:29 -0500

    in chapter 8.2.2

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
   before_save { self.email = email.downcase }
+  before_create :create_remember_token
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(?:\.[a-z\d]+)*\.[a-z]+\z/i
 
@@ -10,6 +11,18 @@ class User < ActiveRecord::Base
   has_secure_password
   validates :password, length: { minimum: 6 }
 
-  
+  def User.new_remember_token
+    SecureRandom.urlsafe_base64           # Generates a token
+  end
 
+  def User.hash(token)
+    Digest::SHA1.hexdigest(token.to_s)    # Hashes a token
+  end
+
+  private
+
+    # Assigns a generated and hashed token to the User.remember_token
+    def create_remember_token
+      self.remember_token = User.hash(User.new_remember_token) 
+    end  
 end
