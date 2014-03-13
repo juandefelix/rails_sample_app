511093cf202ce2a66e3278e655b18d8b9fb922e9
Juan Ortiz <juandefelix@yahoo.es>
2014-03-13 14:14:30 -0500

    Chapter 8.2.3

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,16 @@
 module SessionsHelper
+
+  def sign_in(user)
+    # Create a new token
+    remember_token = User.new_remember_token
+
+    # Place the token in the cookie
+    cookies.permanent[:remember_token] = remember_token
+
+    # Save in the db the hashed remember_token
+    user.update_attribute(:remember_token, User.hash(remember_token))
+
+    # Set the current user to 'user' variable
+    self.current_user = user
+  end
 end
