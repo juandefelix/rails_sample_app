91a5cea0380d550636db019892ee7f1ad9d0af57
Juan Ortiz <juandefelix@yahoo.es>
2014-03-17 23:45:37 -0500

    begining of chapter 9.3

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -30,13 +30,9 @@ describe "Authentication" do
     describe "with valid information" do
       let(:user) { FactoryGirl.create(:user) }
       before { sign_in user }
-      # before do
-        # fill_in "Email",    with: user.email.upcase
-        # fill_in "Password", with: user.password
-        # click_button "Sign in"
-      # end
 
       it { should have_title(user.name) }
+      it { should have_link('Users',       href: users_path) }
       it { should have_link('Profile',     href: user_path(user)) }
       it { should have_link('Settings',    href: edit_user_path(user)) }
       it { should have_link('Sign out',    href: signout_path) }
@@ -48,4 +44,57 @@ describe "Authentication" do
       end
     end # end with valid ...
   end # end signin
+
+  describe "authorization" do
+
+    describe "for non-signed-in users" do
+      let(:user) { FactoryGirl.create(:user) }
+
+      describe "when attempting to visit a protected page" do
+        before do 
+          visit edit_user_path(user)
+          fill_in "Email", with: user.email
+          fill_in "Password", with: user.password
+          click_button "Sign in"
+        end
+
+        describe "after signing in" do
+
+          it "should render the desired protected page" do
+            expect(page).to have_title('Edit User')
+          end
+        end
+      end
+
+      describe "in the Users controller" do
+
+        describe "visiting the edit page" do
+          before { visit edit_user_path(user) }
+          it { should have_title('Sign in') }
+        end
+
+        describe "submitting to the update action" do
+          before { patch user_path(user) }
+          specify {expect(response).to redirect_to(signin_path) }
+        end
+
+        describe "visiting the user index" do
+          before { visit users_path }
+          it { should have_title('Sign in') }
+        end
+      end # end 'in the users controller'
+    end # end 'for non-signed...'
+
+    describe "as wrong user" do
+      let(:user) { FactoryGirl.create(:user) }
+      let(:wrong_user) { FactoryGirl.create(:user, email: "wrong@example.com") }
+      before { sign_in user, no_capybara: true }
+
+      describe "submitting a GET request to the Users#edit action" do
+        before {get edit_user_path(wrong_user) }
+        specify { expect user_path(wrong_user) }
+        specify { expect(response).to redirect_to(root_url) }
+      end
+    end # 'as wrong user'
+  end # end 'authorization'
 end # end Authentication
